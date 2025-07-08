b4e3cd0b5343fc991ffb929efd52dfedf42e2599
shaaibu7 <shaaibusuleiman9@gmail.com>
2025-07-08 12:46:20 +0100

    chore: refactor cli structure for clap

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use alloy::{
     network::TransactionBuilder,
-    primitives::{Address, U256, address},
+    primitives::{Address, U256},
     providers::{Provider, ProviderBuilder},
     rpc::types::TransactionRequest,
     signers::local::PrivateKeySigner,
@@ -36,8 +36,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let private_key: PrivateKeySigner = private_key.parse()?;
 
     let provider_signer = ProviderBuilder::new()
-            .wallet(private_key)
-            .connect_http(rpc_url.clone());
+        .wallet(private_key)
+        .connect_http(rpc_url.clone());
 
     let match_result = command!()
         .subcommand(
@@ -77,7 +77,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 ),
         )
         .subcommand(
-            Command::new("erc20-token-interaction")
+            Command::new("erc20-token")
                 .arg(
                     Arg::new("token-address")
                         .long("token-address")
@@ -85,44 +85,49 @@ async fn main() -> Result<(), Box<dyn Error>> {
                         .required(true)
                         .help("contract address of erc20 token"),
                 )
-                .arg(
-                    Arg::new("token-holder")
-                        .long("token-holder")
-                        .short('u')
-                        .required(true)
-                        .conflicts_with_all(["token-owner", "token-spender", "token-recipient", "transfer-amount"])
-                        .help("token holder address to check balance"),
+                .subcommand(
+                    Command::new("token-balance").arg(
+                        Arg::new("token-holder")
+                            .long("token-holder")
+                            .short('u')
+                            .required(true)
+                            .help("token holder address to check balance"),
+                    ),
                 )
-                .arg(
-                    Arg::new("token-owner")
-                        .long("token-owner")
-                        .short('o')
-                        .required(true)
-                        .help("token owner address to check for allowance"),
+                .subcommand(
+                    Command::new("token-allowance")
+                        .arg(
+                            Arg::new("token-owner")
+                                .long("token-owner")
+                                .short('o')
+                                .required(true)
+                                .help("token owner address to check for allowance"),
+                        )
+                        .arg(
+                            Arg::new("token-spender")
+                                .long("token-spender")
+                                .short('s')
+                                .required(true)
+                                .help("token spender address to check for allowance"),
+                        ),
                 )
-                .arg(
-                    Arg::new("token-spender")
-                        .long("token-spender")
-                        .short('s')
-                        .required(true)
-                        .help("token spender address to check for allowance"),
-                )
-                .arg(
-                    Arg::new("token-recipient")
-                        .long("token-recipient")
-                        .short('r')
-                        .required(true)
-                        .conflicts_with_all(["token-owner", "token-spender"])
-                        .help("address of token recipient in transfer operation"),
-                )
-                .arg(
-                    Arg::new("transfer-amount")
-                        .long("transfer-amount")
-                        .short('a')
-                        .required(true)
-                        .conflicts_with_all(["token-owner", "token-spender"])
-                        .help("address of token recipient in transfer operation"),
-                )
+                .subcommand(
+                    Command::new("token-transfer")
+                        .arg(
+                            Arg::new("token-recipient")
+                                .long("token-recipient")
+                                .short('r')
+                                .required(true)
+                                .help("address of token recipient in transfer operation"),
+                        )
+                        .arg(
+                            Arg::new("transfer-amount")
+                                .long("transfer-amount")
+                                .short('a')
+                                .required(true)
+                                .help("address of token recipient in transfer operation"),
+                        ),
+                ),
         )
         .about(
             "web3 all in one comprehensive cli tool for interacting with the ethereum blockchain",
@@ -155,7 +160,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
         Some(x) => x.get_one::<String>("amount-to-send").unwrap_or(&empty),
         _ => "empty",
     };
-
 
     let provider = ProviderBuilder::new().connect_http(rpc_url.clone());
 
@@ -202,83 +206,83 @@ async fn main() -> Result<(), Box<dyn Error>> {
     }
 
     // ERC20 TRANSACTION OPERATION
-    let token_transaction = match_result.subcommand_matches("erc20-token-interaction");
+    let token_transaction = match_result.subcommand_matches("erc20-token");
 
     let token_address = match token_transaction {
         Some(x) => x.get_one::<String>("token-address").unwrap_or(&empty),
         _ => "empty",
     };
 
-    // Checking balance
-    let token_holder = match token_transaction {
-        Some(x) => x.get_one::<String>("token-holder").unwrap_or(&empty),
-        _ => "empty",
-    };
-
-    if token_address != "empty" && token_holder != "empty" {
+    // checking token balance
+    if let Some(("erc20-token", erc20_matches)) = match_result.subcommand() {
+        let token_address = erc20_matches.get_one::<String>("token-address").unwrap();
         let token_contract_address = Address::from_str(token_address).unwrap();
-
         let erc20 = ERC20::new(token_contract_address, provider.clone());
 
-        let token_holder_address = Address::from_str(token_holder).unwrap();
-        let token_balance = erc20.balanceOf(token_holder_address).call().await?;
+        if let Some(("token-balance", balance_matches)) = erc20_matches.subcommand() {
+            let token_holder = balance_matches.get_one::<String>("token-holder").unwrap();
+            let token_holder_address = Address::from_str(token_holder).unwrap();
+            let token_balance = erc20.balanceOf(token_holder_address).call().await?;
 
-        println!("Token balance for user is: {}", token_balance);
-    }
-
-    // Checking token allowance
-    let token_owner = match token_transaction {
-        Some(x) => x.get_one::<String>("token-owner").unwrap_or(&empty),
-        _ => "empty",
-    };
-
-    let token_spender = match token_transaction {
-        Some(x) => x.get_one::<String>("token-spender").unwrap_or(&empty),
-        _ => "empty",
-    };
-
-    if token_owner != "empty" && token_spender != "empty" {
-        if token_address != "empty" {
-            let token_contract_address = Address::from_str(token_address).unwrap();
-
-            let erc20 = ERC20::new(token_contract_address, provider);
-
-            let token_owner_address = Address::from_str(token_owner).unwrap();
-            let token_spender_address = Address::from_str(token_spender).unwrap();
-            let token_balance = erc20.allowance(token_owner_address,  token_spender_address).call().await?;
-
-            println!("Token allowance for spender is: {}", token_balance);
-           
+            println!("Token balance for user is: {}", token_balance);
         }
     }
 
+    // Checking token allowance
+
+    if let Some(("erc20-token", erc20_matches)) = match_result.subcommand() {
+        let token_address = erc20_matches.get_one::<String>("token-address").unwrap();
+        let token_contract_address = Address::from_str(token_address).unwrap();
+        let erc20 = ERC20::new(token_contract_address, provider.clone());
+
+        if let Some(("token-allowance", balance_matches)) = erc20_matches.subcommand() {
+            let token_owner = balance_matches.get_one::<String>("token-owner").unwrap();
+            let token_spender = balance_matches.get_one::<String>("token-spender").unwrap();
+
+            let token_owner_address = Address::from_str(token_owner).unwrap();
+            let token_spender_address = Address::from_str(token_spender).unwrap();
+
+            let token_balance = erc20
+                .allowance(token_owner_address, token_spender_address)
+                .call()
+                .await?;
+
+            println!("Token allowance for spender is: {}", token_balance);
+        }
+    }
 
     // Integrating erc20 token contract write ops
-    let token_recipient = match token_transaction {
-        Some(x) => x.get_one::<String>("token-recipient").unwrap_or(&empty),
-        _ => "empty",
-    };
 
-    let transfer_amount = match token_transaction {
-        Some(x) => x.get_one::<String>("transfer-amount").unwrap_or(&empty),
-        _ => "empty",
-    };
-
-
-    if token_recipient != "empty" && transfer_amount != "empty" && token_address != "empty" {
-        let transfer_amount_transform = U256::from_str_radix(transfer_amount, 10).unwrap();
-        let transfer_data = transfer_amount_transform.wrapping_mul(U256::from(10e5));
+    if let Some(("erc20-token", erc20_matches)) = match_result.subcommand() {
+        let token_address = erc20_matches.get_one::<String>("token-address").unwrap();
         let token_contract_address = Address::from_str(token_address).unwrap();
+        let erc20 = ERC20::new(token_contract_address, provider_signer.clone());
 
-        println!("The transformed data is {}", transfer_data);
+        if let Some(("token-transfer", balance_matches)) = erc20_matches.subcommand() {
+            let token_recipient = balance_matches
+                .get_one::<String>("token-recipient")
+                .unwrap();
+            let transfer_amount = balance_matches
+                .get_one::<String>("transfer-amount")
+                .unwrap();
 
-        let erc20 = ERC20::new(token_contract_address, provider_signer);
+            let transfer_amount_transform = U256::from_str_radix(transfer_amount, 10).unwrap();
+            let transfer_data = transfer_amount_transform.wrapping_mul(U256::from(10e5));
+            let token_recipient_transform = Address::from_str(token_recipient).unwrap();
 
-        let token_recipient_transform = Address::from_str(token_recipient).unwrap();
-        let transfer_tx = erc20.transfer(token_recipient_transform, transfer_data).send().await?;
-        let reciept_tx = transfer_tx.get_receipt().await?;
+            let transfer_tx = erc20
+                .transfer(token_recipient_transform, transfer_data)
+                .send()
+                .await?;
+            let reciept_tx = transfer_tx.get_receipt().await?;
 
-        println!("Transfer transaction completed successfully and tx hash is {}", reciept_tx.transaction_hash);
+            println!(
+                "Transfer transaction completed successfully and tx hash is {}",
+                reciept_tx.transaction_hash
+            );
+
+            println!("{} {}", token_recipient, transfer_amount);
+        }
     }
 
     Ok(())
