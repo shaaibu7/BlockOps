42b5efe004d1e7882aa833e9a4879038902b81ec
dimka90 <dimkayilrit@gmail.com>
2025-07-11 09:09:47 +0100

    feat: added functionality for swapping tokens on uniswap

--- src/utils.rs
+++ src/utils.rs
@@ -7,6 +7,15 @@ use alloy::{
 use std::error::Error;
 use std::str::FromStr;
 
+
+use std::{collections::HashMap, env};
+use clap::{ArgMatches};
+use alloy::{primitives::{U256}, signers::k256::ecdsa::SigningKey};
+use alloy::{hex::decode};
+use alloy::signers::local::LocalSigner;
+use dotenv::dotenv;
+
+
 /// Setup provider with signer for transactions
 pub async fn setup_provider_with_signer(
     rpc_url: &Url,
@@ -49,3 +58,88 @@ pub fn format_balance(balance: u64, decimals: u8) -> f64 {
     let divisor = 10_f64.powi(decimals as i32);
     balance as f64 / divisor
 } 
+
+
+pub fn get_address_from_private_key(private_key_hex: &str) -> Result<Address, String> {
+
+    let key_str = private_key_hex.trim_start_matches("0x");
+    let key_bytes = decode(key_str).map_err(|_| "Invalid hex string".to_string())?;
+    if key_bytes.len() != 32 {
+        return Err("Private key must be 32 bytes".to_string());
+    }
+    let mut key_array = [0u8; 32];
+    key_array.copy_from_slice(&key_bytes);
+    let signing_key = SigningKey::from_bytes((&key_array).into())
+        .map_err(|_| "Invalid private key bytes".to_string())?;
+    let signer = LocalSigner::from_signing_key(signing_key);
+    Ok(signer.address())
+}
+#[derive(Debug)]
+  pub  struct SwapConfig{
+    pub token_in: Address,
+    pub token_out: Address,
+    pub amount_in: U256,
+    pub fee_tier: U256,
+    pub slippage_bps: u16,
+    pub deadline_seconds: u64,
+    pub recipient: Address,
+    pub sqrt_price_limit: U256,
+    pub private_key: String
+}
+
+pub fn parse_swap_config(matches: &ArgMatches)  -> Option< SwapConfig>{
+    
+if let (Some(token_a),
+        Some(token_b),
+        Some(amount),
+        Some(fee_tier),
+        Some(slippage_bps),
+        Some(deadline),
+        Some(private_key)
+    ) = (
+    matches.get_one::<String>("token-in"),
+    matches.get_one::<String>("token-out"),
+    matches.get_one::<String>("amount-in"),
+    matches.get_one::<String>("fee-tier"),
+    matches.get_one::<String>("slippage-bps"),
+    matches.get_one::<String>("deadline"),
+    matches.get_one::<String>("private-key")
+    ) {
+
+    let user_address = get_address_from_private_key(&private_key).ok().unwrap();
+
+    let token_address = load_tokens_address_from_env();
+
+
+let config = SwapConfig{
+    token_in: token_address.get(token_a.as_str()).unwrap().parse().expect("Invalid token Address for token a"),
+    token_out: token_address.get(token_b.as_str()).unwrap().parse().expect("Invalid token Address for token b"),
+    amount_in: U256::from(amount.parse::<u64>().expect("Invalid amount") * 1_000_000), // Convert to proper decimals for USDC
+    fee_tier: fee_tier.parse().expect("Invalid fee tier"),
+    slippage_bps: slippage_bps.parse().expect("Invalid slippage"),
+    deadline_seconds: deadline.parse().expect("invalid deadline"),
+    recipient: user_address,
+    sqrt_price_limit: U256::ZERO,
+    private_key:  private_key.parse().expect("invalid deadline")
+
+};
+
+return  Some(config);
+}
+None
+}
+
+
+
+pub fn load_tokens_address_from_env() -> HashMap< &'static str, String>{
+      dotenv().ok();
+    let usdc = env::var("USDC").unwrap_or_else(|_| panic!("USDC ADDRESS NOT FOUND"));
+    let weth  = env::var("WETH").unwrap_or_else(|_| panic!("WTH ADDRESS NOT FOUND"));
+
+    let mut tokens = HashMap::new();
+    tokens.insert("USDC", usdc);
+    tokens.insert("WETH", weth);
+
+    tokens
+
+}
